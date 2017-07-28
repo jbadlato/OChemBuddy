e5a7382b8aa0abad0ed7a0c4b0f2e8017c482b55
Joseph Badlato <jbadlato@vt.edu>
2017-07-28 15:25:19 -0400

    Change longest path function to output the path instead of length

--- script.js
+++ script.js
@@ -6,12 +6,120 @@ function test() {
 	console.log(out);
 	
 	
-	/*//FUNCTION: findLongestPath(adjList)
+	//FUNCTION: findLongestPath(adjList)
 	var smiles = document.getElementById("SMILES").value;
 	graph = smilesToCarbonSkeleton(smiles);
 	out = findLongestPath(graph);
 	console.log(out);
-	*/
+	
+}
+
+var numToPrefix = {
+	1: "meth",
+	2: "eth",
+	3: "prop",
+	4: "but",
+	5: "pent",
+	6: "hex",
+	7: "hept",
+	8: "oct",
+	9: "non",
+	10: "dec",
+	11: "undec",
+	12: "dodec",
+	13: "tridec",
+	14: "tetradec",
+	15: "pentadec",
+	16: "hexadec",
+	17: "heptadec",
+	18: "octadec",
+	19: "nonadec",
+	20: "eicos"
+}
+
+function getMaxOfArray(numArray) {
+	return Math.max.apply(null, numArray);
+}
+
+function arrayContains(arr1, arr2) { // Returns true if arr1 contains every element of arr2. Returns false otherwise.
+	for (var i = 0; i < arr2.length; i++) {
+		if (arr1.indexOf(arr2[i]) === -1) {
+			return false;
+		}
+	}
+	return true;
+}
+
+function findLongestPath(adjList) { // returns length of longest path possible without repeating nodes
+
+	// ********** NOTE: THIS RETURNS THE LENGTH OF THE PATH. THIS TRANSLATES TO THE NUMBER OF BONDS, NOT NUMBER OF CARBONS ALONG THE PATH. ************
+
+
+	endPts = []; // list of nodes with only one edge
+	for (var i = 0; i < adjList.length; i++) {
+		if (adjList[i].length === 1) {
+			endPts.push(i);
+		}
+	}
+
+	longestPathLength = 0;
+	for (var i = 0; i < endPts.length; i++) {
+		startPt = endPts[i];
+		pathLengths = new Array(adjList.length).fill(-1);
+		searched = [startPt];
+		distance = 0;
+		pathLengths[startPt] = 0;
+		while (searched.length < adjList.length) {
+			distance++;
+			newNodes = [];
+			for (var j = 0; j < searched.length; j++) {
+				for (var k = 0; k < adjList[searched[j]].length; k++) {
+					h = adjList[searched[j]][k];
+					if (newNodes.indexOf(h) === -1 && searched.indexOf(h) === -1) {
+						newNodes.push(h);
+					}
+					if (pathLengths[h] === -1) {
+						pathLengths[h] = distance;
+					}
+
+				}
+			}
+			searched = searched.concat(newNodes);
+		}
+		if (getMaxOfArray(pathLengths) > longestPathLength) {
+			carbon1 = endPts[i];
+			carbonLast = pathLengths.indexOf(getMaxOfArray(pathLengths));
+		}
+		longestPathLength = Math.max(longestPathLength, getMaxOfArray(pathLengths));
+	}
+
+	// Find the actual path, not just the length:
+	discovered = [carbon1];
+	testPath = [carbon1];
+	currNode = carbon1;
+	while (testPath.length < longestPathLength+1) {
+		for (var k = 1; k < (testPath.length - 1); k++) { // Delete nodes from test path except the end point and the start point
+			ind = discovered.indexOf(testPath[k]);
+			discovered.splice(ind, 1);
+		}
+		testPath = [carbon1];
+		currNode = carbon1;
+		while (!arrayContains(discovered, adjList[currNode])) { // While there is an undiscovered node connected to current node THIS LOOP IS INFINITE	
+			for (var j = 0; j < adjList[currNode].length; j++) {
+				x = adjList[currNode][j];
+				if (discovered.indexOf(x) === -1) { // Only if we haven't used the node yet
+					discovered.push(x);
+					testPath.push(x);
+					currNode = x;
+					break;
+				}
+			}
+		}
+
+	}
+
+
+	return testPath;
 }
 
 function xParenth(s) {	
@@ -120,48 +228,14 @@ function smilesToCarbonSkeleton(smilesInput) {
 	return adjacencyList;
 }
 
-function findLongestPath(adjList) { // returns length of longest path possible without repeating nodes
+function findBranches(skel) {
+	longChain = findLongestPath(skel) + 1;
 
-	// ********** NOTE: THIS RETURNS THE LENGTH OF THE PATH. THIS TRANSLATES TO THE NUMBER OF BONDS, NOT NUMBER OF CARBONS ALONG THE PATH. ************
-
-
-	endPts = []; // list of nodes with only one edge
-	for (var i = 0; i < adjList.length; i++) {
-		if (adjList[i].length === 1) {
-			endPts.push(i);
-		}
-	}
-
-	longestPath = 0;
-	for (var i = 0; i < endPts.length; i++) {
-		startPt = endPts[i];
-		pathLengths = new Array(adjList.length).fill(-1);
-		searched = [startPt];
-		distance = 0;
-		pathLengths[startPt] = 0;
-		while (searched.length < adjList.length) {
-			distance++;
-			newNodes = [];
-			for (var j = 0; j < searched.length; j++) {
-				for (var k = 0; k < adjList[searched[j]].length; k++) {
-					h = adjList[searched[j]][k];
-					if (newNodes.indexOf(h) === -1 && searched.indexOf(h) === -1) {
-						newNodes.push(h);
-					}
-					if (pathLengths[h] === -1) {
-						pathLengths[h] = distance;
-					}
-
-				}
-			}
-			searched = searched.concat(newNodes);
-		}
-		longestPath = Math.max(longestPath, getMaxOfArray(pathLengths));
-	}
-
-	return longestPath;
 }
 
-function getMaxOfArray(numArray) {
-	return Math.max.apply(null, numArray);
+function name() {
+	var input = document.getElementById("SMILES").value;
+	skeleton = smilesToCarbonSkeleton(input);
+	longestChain = findLongestPath(skeleton) + 1;
+	
 }
