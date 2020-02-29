4b6599a45877436d7d73cc3c4b59b7af404c5219
Joe Badlato <joebadlato@gmail.com>
2020-02-28 22:43:55 -0500

    React POC

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App() {
+	return (
+			<div className="App">
+				<iframe id="ifKetcher" title="ketcher" src="ketcher/ketcher.html" width="800" height="600"></iframe>
+			</div>
+		);
+}
+
+export default App;
